635b5663c2446cdd4fad296321e549a25caa33a2
tiagoc0sta <tiagodpv@gmail.com>
2024-07-14 15:37:35 -0400

    update pictures

--- C/ViewControllers/CarsViewController.swift
+++ C/ViewControllers/CarsViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class CarsViewController: UIViewController {
     
+    private var filteredCars: [Car] = [] //initialize variables to filter cars on the research bar
+    private var isSearchActive: Bool = false
+    
     // creating the table to display the cars
     private lazy var tableView: UITableView = {
        let tableView = UITableView()
@@ -18,6 +21,16 @@ class CarsViewController: UIViewController {
         tableView.delegate = self // defines that CarsViewController defines the behaivour of the table
         tableView.register(CarTableViewCell.self, forCellReuseIdentifier: "carCell")
         return tableView
+    }()
+    
+    //creating searchingBar
+    private lazy var searchBar: UISearchBar = {
+        let searchBar = UISearchBar()
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
+        searchBar.placeholder = "research"
+        searchBar.searchTextField.backgroundColor = .white
+        searchBar.delegate = self
+        return searchBar
     }()
     
 
@@ -47,6 +60,7 @@ class CarsViewController: UIViewController {
         navigationController?.navigationBar.prefersLargeTitles = true // change title size
         navigationController?.navigationBar.largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor : UIColor.white] // Change title color
         navigationItem.setHidesBackButton(true, animated: true) //delete  "back" button
+        navigationItem.titleView = searchBar
     }
 
 }
@@ -55,12 +69,13 @@ class CarsViewController: UIViewController {
 extension CarsViewController: UITableViewDataSource, UITableViewDelegate {
     // mandatory method when using UITableViewDataSource - related to data in a table
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return cars.count
+        return isSearchActive ? filteredCars.count : cars.count
     }
     // mandatory method when using UITableViewDataSource - related to data in a table
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if let cell = tableView.dequeueReusableCell(withIdentifier: "carCell", for: indexPath) as? CarTableViewCell {
-            cell.configureCell(car: cars[indexPath.row]) //calling configureCell function from CarTableViewCell
+            let car = isSearchActive ? filteredCars[indexPath.row] : cars [indexPath.row] //define wich array to use base on search active ot not
+            cell.configureCell(car: car) //calling configureCell function from CarTableViewCell
             cell.selectionStyle = .none // disable shadow when press button
             return cell
         }
@@ -70,12 +85,28 @@ extension CarsViewController: UITableViewDataSource, UITableViewDelegate {
     // method acts when selecting a cell
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
-        let detailsVC = CarDetailsViewController(car: cars[indexPath.row])
+        let car = isSearchActive ? filteredCars[indexPath.row] : cars [indexPath.row] //define wich array to use base on search active ot not
+        let detailsVC = CarDetailsViewController(car: car)
         navigationController?.pushViewController(detailsVC, animated: true)
     }
     
     // define table height
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 160
+    }
+}
+
+//capturing input from user on search bar and filtering
+extension CarsViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchText.isEmpty {
+            isSearchActive = false
+        } else {
+            isSearchActive = true
+            filteredCars =  cars.filter({ car in
+                car.title.lowercased().contains(searchText.lowercased())
+            })
+        }
+        tableView.reloadData()
     }
 }
